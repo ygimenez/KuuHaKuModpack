6cdf8b22ab0f7b683e5d4f5aab58fe6679d25ef9
yagog <Yago1234!!khk>
2022-06-03 19:27:10 -0300

    Update

--- mc-server/run.bat
+++ mc-server/run.bat
@@ -4,6 +4,7 @@
 :: Add custom JVM arguments to the user_jvm_args.txt
 :: Add custom program arguments (such as nogui) to this file in the next line before the %* or pass them to this script directly
 
+:jvm_config
 SET /P JDK=<java_path.txt
 
 IF "%JDK%"=="" (
